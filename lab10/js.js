93538cf4bed386398b90067d9d1adcf6d02e0f22
Yevhen <ytupychka2022@itcollege.lviv.ua>
2026-01-13 21:25:16 +0200

    labs

--- lab10/js.js
+++ lab10/js.js
@@ -1,56 +1,74 @@
-class Person {
-    constructor(name) {
-        this.name = name;
-    }
+class Product {
+  constructor(name, price, category) {
+    this.name = name;
+    this.price = price;
+    this.category = category;
+  }
 
-    getInfo() {
-        return `Ім'я: ${this.name}`;
-    }
+  getInfo() {
+    return `${this.name} (${this.category}) - ${this.price} грн`;
+  }
 }
-class Course {
-    constructor(title) {
-        this.title = title;
-        this.students = [];
-    }
+class User {
+  constructor(name) {
+    this.name = name;
+  }
 
-    addStudent(student) {
-        this.students.push(student);
-        student.courses.push(this);
-    }
-}
-class Teacher extends Person {
-    constructor(name) {
-        super(name);
-        this.courses = [];
-    }
-
-    addCourse(course) {
-        this.courses.push(course);
-    }
+  getUserInfo() {
+    return `Користувач: ${this.name}`;
+  }
 }
 
-class Student extends Person {
-    constructor(name) {
-        super(name);
-        this.courses = [];
+class Customer extends User {
+  constructor(name) {
+    super(name);
+    this.orders = [];
+  }
+  addOrder(order) {
+    this.orders.push(order);
+  }
+  viewOrders() {
+    if (this.orders.length === 0) {
+      console.log("Замовлень немає");
+      return;
     }
 
-    viewCourses() {
-        return this.courses.map(c => c.title);
-    }
+    this.orders.forEach((order, index) => {
+      console.log(`Замовлення №${index + 1}:`);
+      order.showOrder();
+    });
+  }
 }
-const js = new Course("JavaScript");
-const html = new Course("HTML & CSS");
-const teacher = new Teacher("Ігор Тупичак");
-teacher.addCourse(js);
-teacher.addCourse(html);
-const s1 = new Student("Давид");
-const s2 = new Student("Арсен");
-js.addStudent(s1);
-js.addStudent(s2);
-html.addStudent(s1);
-console.log(teacher.getInfo());
-console.log("Курси викладача:", teacher.courses.map(c => c.title));
-console.log(s1.getInfo());
-console.log("Курси студента:", s1.viewCourses());
-console.log("Студенти на курсі JavaScript:", js.students.map(s => s.name));
+class Order {
+  constructor() {
+    this.products = [];
+    this.totalSum = 0;
+  }
+
+  addProduct(product) {
+    this.products.push(product);
+    this.totalSum += product.price;
+  }
+
+  showOrder() {
+    this.products.forEach(product => {
+      console.log(product.getInfo());
+    });
+    console.log(`Загальна сума: ${this.totalSum} грн`);
+    console.log("--------------------");
+  }
+}
+
+const product1 = new Product("Ноутбук", 25000, "Електроніка");
+const product2 = new Product("Мишка", 500, "Аксесуари");
+const product3 = new Product("Клавіатура", 1200, "Аксесуари");
+const customer = new Customer("Іван");
+const order1 = new Order();
+order1.addProduct(product1);
+order1.addProduct(product2);
+const order2 = new Order();
+order2.addProduct(product3);
+customer.addOrder(order1);
+customer.addOrder(order2);
+console.log(customer.getUserInfo());
+customer.viewOrders();
--- lab8/main.js
+++ lab8/main.js
@@ -1,14 +1,59 @@
-function processSet(set, callback) {
-  if (!(set instanceof Set)) {
-    throw new Error("Перший аргумент має бути Set");
-  }
-  
 
-  for (let item of set) {
-    callback(item);
-  }
+function findMax(a, b) {
+  return a > b ? a : b;
 }
-const mySet = new Set([1, 2, 3, 4]);
-processSet(mySet, (el) => {
-  console.log(el * 2);
-});
+console.log(findMax(5, 10)); 
+const subtract = function(a, b) {
+  return a - b;
+};
+
+console.log(subtract(10, 3)); 
+const sqrt = (n) => Math.sqrt(n);
+console.log(sqrt(16)); 
+
+function geometricProgression(n, a, r) {
+  if (n === 1) {
+    return a;
+  }
+  return a * Math.pow(r, n - 1) + geometricProgression(n - 1, a, r);
+}
+
+console.log(geometricProgression(3, 2, 2)); 
+function createDivider(divisor) {
+  return function(number) {
+    return number / divisor;
+  };
+}
+
+const divideBy2 = createDivider(2);
+console.log(divideBy2(10)); 
+
+
+function applyOperation(a, b, func) {
+  return func(a, b);
+}
+
+function add(x, y) {
+  return x + y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+
+console.log(applyOperation(3, 4, add));      
+console.log(applyOperation(3, 4, multiply)); 
+
+function processSet(set, callback) {
+  const result = new Set();
+  for (let item of set) {
+    result.add(callback(item));
+  }
+  return result;
+}
+
+const numbersSet = new Set([1, 2, 3, 4]);
+const squaredSet = processSet(numbersSet, x => x * x);
+
+console.log(squaredSet); 
